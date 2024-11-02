9f9c268fb2b455928e7439f7f05c81ce5e7d6751
SF-Saikat <saikat@stockforecast.in>
2024-11-03 01:01:06 +0530

    vercel 4

--- app.js
+++ app.js
@@ -25,21 +25,22 @@ app.use(bodyparser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 
 //load assets
-app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
-app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
-// app.use('/output',express.static(path.resolve(__dirname,"views/output")))
-app.use('/images',express.static(path.resolve(__dirname,"assets/images")))
-app.use('/fonts',express.static(path.resolve(__dirname,"assets/fonts")))
-app.use('/includes',express.static(path.resolve(__dirname,"views/includes")))
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")));
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")));
+// app.use('/output',express.static(path.resolve(__dirname,"views/output")));
+app.use('/images',express.static(path.resolve(__dirname,"assets/images")));
+app.use('/fonts',express.static(path.resolve(__dirname,"assets/fonts")));
+app.use('/includes',express.static(path.resolve(__dirname,"views/includes")));
 
 //routes
-app.use('/',require('./server/routes/router'))
+app.use('/',require('./server/routes/router'));
 
 
-app.listen(port,()=>{  // do not add localhost here if you are deploying it
-    console.log("server listening to port "+port);
-});
+// app.listen(port,()=>{  // do not add localhost here if you are deploying it
+//     console.log("server listening to port "+port);
+// });
 
+module.exports = app;
 
 
 
